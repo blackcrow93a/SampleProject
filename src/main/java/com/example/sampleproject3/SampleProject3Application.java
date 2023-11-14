c053b4cd73a9a0055b848851260ba63ffb061531
sooyoung <jtndudz3@gmail.com>
2023-11-14 23:49:20 +0900

    test

---
+++ src/main/java/com/example/sampleproject3/SampleProject3Application.java
@@ -0,0 +1,13 @@
+package com.example.sampleproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProject3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleProject3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sampleproject3/SampleProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleproject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleProject3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
